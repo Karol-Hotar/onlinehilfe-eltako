6dad94d5df47d30ae0b645f8173be27ad0227299
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 11:08:55 +0200

    jekyll build

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.1105';
+const cacheName = 'chirpy-20210629.1107';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
