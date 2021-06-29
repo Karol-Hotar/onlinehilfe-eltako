c17ebfffcd19fd5d9978dbd0a7f6dc1240bb4fb0
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 08:27:15 +0200

    Initial Commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.0625';
+const cacheName = 'chirpy-20210629.0626';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
