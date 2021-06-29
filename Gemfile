9ef07b0beb8837d21f8d5ae15a1238e6da82b82a
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 11:03:49 +0200

    cnf

--- Gemfile
+++ Gemfile
@@ -28,8 +28,3 @@ gem "webrick", "~> 1.7"
 
 gem "livereload", "~> 1.6"
 
-gem "jekyll"
-
-gem "github-pages"
-
-gem "sprockets", "~> 3.7"
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.0938';
+const cacheName = 'chirpy-20210629.1103';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
