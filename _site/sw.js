0d660c9210b1d91d1b6c733d01c7b284e1297b20
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 12:50:11 +0200

    changelog

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.1247';
+const cacheName = 'chirpy-20210629.1250';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
