b0866bf4824b6d5955d7aa8476a14f800ef2537b
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 13:21:46 +0200

    navigation hover fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.1314';
+const cacheName = 'chirpy-20210629.1321';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
