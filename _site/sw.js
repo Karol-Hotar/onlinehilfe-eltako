ffb33a6e18ec0039d1b13b335a205610f92e4148
Karol Hotár <hotar.karol@gmail.com>
2021-06-29 08:41:40 +0200

    Config UTF

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210629.0635';
+const cacheName = 'chirpy-20210629.0641';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
